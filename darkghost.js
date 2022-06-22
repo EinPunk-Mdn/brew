b8e4381d12d5e2d66d3f2a61a5e55f98d49fc40a
EinPunk <100175732+EinPunk-Mdn@users.noreply.github.com>
2022-06-22 19:32:14 +0100

    DarkGhost

---
+++ darkghost.js
@@ -0,0 +1,13 @@
+ 
+{
+  "name": "DarkGhost",
+  "version": "1.0.0",
+  "description": "My New Project description.",
+  "main": "darkghost.js",
+  "repository": {
+    "url": "https://github.com/EinPunk-Mdn/DarkGhost",
+    "type": "git"
+  },
+  "author": "DarkGhost <darkghost@github.com>",
+  "license": "MIT"
+}
